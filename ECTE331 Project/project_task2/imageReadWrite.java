2f6299378c47ef44bdf1b9f6f19c97f555148d94
Muaadh Shihabdeen <55185634+mshihabdeen@users.noreply.github.com>
2025-06-27 00:56:31 +0530

    Update imageReadWrite.java

--- ECTE331 Project/project_task2/imageReadWrite.java
+++ ECTE331 Project/project_task2/imageReadWrite.java
@@ -8,38 +8,38 @@ import javax.imageio.ImageIO;
 
 class imageReadWrite{
 
-	public static void readJpgImage(String filePath, colourImage imgData) {
+	public static void readJpgImage(String fileName, colourImage ImgStruct) {
 		 try {
 	            // Read the image file
-	            File imageFile = new File(filePath);
-	            BufferedImage bufferedImg = ImageIO.read(imageFile);
+	            File file = new File(fileName);
+	            BufferedImage image = ImageIO.read(file);
 	            
-	            System.out.println("file: "+imageFile.getCanonicalPath());
+	            System.out.println("file: "+file.getCanonicalPath());
 	            
 	            // Check if the image is in sRGB color space
-	            if (!bufferedImg.getColorModel().getColorSpace().isCS_sRGB()) {
+	            if (!image.getColorModel().getColorSpace().isCS_sRGB()) {
 	                System.out.println("Image is not in sRGB color space");
 	                return;
 	            }
 	            
 	            // Get the width and height of the image
-	            int imgWidth = bufferedImg.getWidth();
-	            int imgHeight = bufferedImg.getHeight();
-	            imgData.imgWidth = imgWidth;
-	            imgData.imgHeight = imgHeight;
-	            imgData.rgbData = new short[imgHeight][imgWidth][3];
+	            int width = image.getWidth();
+	            int height = image.getHeight();
+	            ImgStruct.width=width;
+	            ImgStruct.height=height;
+	            ImgStruct.pixels=new short[height][width][3];
 
 	           // Loop over each pixel of the image and store its RGB color components in the array
-	            for (int row = 0; row < imgHeight; row++) {
-	                for (int col = 0; col < imgWidth; col++) {
+	            for (int y = 0; y < height; y++) {
+	                for (int x = 0; x < width; x++) {
 	                    // Get the color of the current pixel
-	                    int pixelValue = bufferedImg.getRGB(col, row);
-	                    Color pixelColor = new Color(pixelValue, true);
+	                    int pixel = image.getRGB(x, y);
+	                    Color color = new Color(pixel, true);
 
 	                    // Store the red, green, and blue color components of the pixel in the array
-	                    imgData.rgbData[row][col][0] = (short) pixelColor.getRed();
-	                    imgData.rgbData[row][col][1] = (short) pixelColor.getGreen();
-	                    imgData.rgbData[row][col][2] = (short) pixelColor.getBlue();
+	                    ImgStruct.pixels[y][x][0] = (short) color.getRed();
+	                    ImgStruct.pixels[y][x][1] = (short) color.getGreen();
+	                    ImgStruct.pixels[y][x][2] = (short) color.getBlue();
 	                }
 	            }            
 	                       
@@ -49,23 +49,23 @@ class imageReadWrite{
 	        }  	
 	}
 
-	public static void writeJpgImage(colourImage imgData, String filePath) {
+	public static void writeJpgImage(colourImage ImgStruct, String fileName) {
 		 try {
-	    	 int imgWidth = imgData.imgWidth;
-	         int imgHeight = imgData.imgHeight;
-	         BufferedImage bufferedImg = new BufferedImage(imgWidth, imgHeight, BufferedImage.TYPE_INT_RGB);
+	    	 int width = ImgStruct.width;
+	         int height = ImgStruct.height;
+	         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
 	         // Set the RGB color values of the BufferedImage using the pixel array
-	         for (int row = 0; row < imgHeight; row++) {
-	             for (int col = 0; col < imgWidth; col++) {
-	                 int rgbValue = new Color(imgData.rgbData[row][col][0], imgData.rgbData[row][col][1], imgData.rgbData[row][col][2]).getRGB();
-	                 bufferedImg.setRGB(col, row, rgbValue);
+	         for (int y = 0; y < height; y++) {
+	             for (int x = 0; x < width; x++) {
+	                 int rgb = new Color(ImgStruct.pixels[y][x][0], ImgStruct.pixels[y][x][1], ImgStruct.pixels[y][x][2]).getRGB();
+	                 image.setRGB(x, y, rgb);
 	             }
 	         }
 
 	         // Write the BufferedImage to a JPEG file
-	         File outputFile = new File(filePath);
-	         ImageIO.write(bufferedImg, "jpg", outputFile);
+	         File outputFile = new File(fileName);
+	         ImageIO.write(image, "jpg", outputFile);
 
 	     } catch (IOException e) {
 	         System.out.println("Error writing image file: " + e.getMessage());
